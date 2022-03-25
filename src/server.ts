3622bcd0bc52e31c72c8a7a7ce2778a5d90947fd
Jean Pierre <pgdick@gmail.com>
2022-03-25 12:45:22 -0300

    fix: test heroku start server

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,14 @@ import app from './app'
 import connectionConfig from './database/ormconfig'
 import 'dotenv/config'
 
-console.log(process.env.PORT)
-async function main(): Promise<void> {
-  console.log(process.env.PORT)
-  await createConnection(connectionConfig)
+console.log('heroku port******', process.env.PORT)
+createConnection(connectionConfig).then(() => {
   const port = process.env.PORT || 5000
   app.listen(port, () =>
     console.log(
       `Server Start On Port ${port}`
     )
   )
-}
+}).catch(console.log)
 
-main().catch(console.log)
+
